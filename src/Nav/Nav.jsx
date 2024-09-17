9ff22983f771e625af30f17f5fd9ecea3bbaf544
kpbongat <kpbongat.dev@gmail.com>
2024-09-17 22:10:32 +0800

    Add basic styling to nav bar

--- src/Nav/Nav.jsx
+++ src/Nav/Nav.jsx
@@ -1,16 +1,24 @@
+import styles from "./Nav.module.css";
 import { Link } from "react-router-dom";
+
 function Nav() {
   return (
-    <nav>
-      <ul>
+    <nav className={styles.nav}>
+      <ul className={styles.ul}>
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/" className={styles.link}>
+            Home
+          </Link>
         </li>
         <li>
-          <Link to="shop">Shop</Link>
+          <Link to="shop" className={styles.link}>
+            Shop
+          </Link>
         </li>
         <li>
-          <Link to="cart">Cart</Link>
+          <Link to="cart" className={styles.link}>
+            Cart
+          </Link>
         </li>
       </ul>
     </nav>
