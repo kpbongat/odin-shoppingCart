172af6b3bb93c4c52ebb1a3d9972fb31cf75cda6
kpbongat <kpbongat.dev@gmail.com>
2024-09-23 17:56:51 +0800

    Create Shop mock dynamically to customize states

--- src/__tests__/Shop.test.jsx
+++ src/__tests__/Shop.test.jsx
@@ -2,31 +2,37 @@ import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 import { vi } from "vitest";
 import Product from "../Product/Product";
-import Shop from "../Shop/Shop";
+
+const mockShop = async ([productState, loadingState]) => {
+  vi.doMock("../Shop/Shop", () => ({
+    default: () => {
+      const products = productState;
+      const loading = loadingState;
+      const cart = {};
+      const setCart = vi.fn();
+      if (loading) {
+        return <section>Loading products...</section>;
+      }
+      return (
+        <section>
+          {products.map((i) => (
+            <Product key={i.id} product={i} cart={cart} setCart={setCart} />
+          ))}
+        </section>
+      );
+    },
+  }));
+  return (await import("../Shop/Shop")).default;
+};
 
 describe("product list", () => {
-  it("renders correct number of products", () => {
-    vi.mock("../Shop/Shop", () => ({
-      default: () => {
-        const products = [
-          { id: 1, title: "test1" },
-          { id: 2, title: "test2" },
-        ];
-        const loading = false;
-        const cart = {};
-        const setCart = vi.fn();
-        if (loading) {
-          return <section>Loading products...</section>;
-        }
-        return (
-          <section>
-            {products.map((i) => (
-              <Product key={i.id} product={i} cart={cart} setCart={setCart} />
-            ))}
-          </section>
-        );
-      },
-    }));
+  it("renders correct number of products", async () => {
+    const products = [
+      { id: 1, title: "test1" },
+      { id: 2, title: "test2" },
+    ];
+    const loading = false;
+    const Shop = await mockShop([products, loading]);
     render(<Shop />);
     expect(screen.queryAllByTestId("product").length).toBe(2);
   });
