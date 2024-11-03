c8f03f63c4936baea00b8a79d800f9b753ac2b2b
kpbongat <kpbongat.dev@gmail.com>
2024-11-03 15:30:42 +0800

    Add stock banner images to home page

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,7 +1,11 @@
+import styles from "./Home.module.css";
+import saleBanner1 from "../../public/assets/saleBanner1.jpg";
+import saleBanner2 from "../../public/assets/saleBanner2.jpg";
 function Home() {
   return (
-    <section>
-      <header>Test Home</header>
+    <section className={styles.section}>
+      <img src={saleBanner1} alt="" width="1000px" height="auto" />
+      <img src={saleBanner2} alt="" width="1000px" height="auto" />
     </section>
   );
 }
--- src/Nav/Nav.jsx
+++ src/Nav/Nav.jsx
@@ -1,6 +1,6 @@
 import styles from "./Nav.module.css";
 import { Link } from "react-router-dom";
-import cartIcon from "../assets/cart-outline.svg";
+import cartIcon from "../../public/assets/cart-outline.svg";
 import PropTypes from "prop-types";
 function Nav({ cartCount }) {
   return (
